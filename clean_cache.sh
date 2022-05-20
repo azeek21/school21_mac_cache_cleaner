8934b6f7c2422c2d5f2b11a00b986ce442bea2e9
Abdulaziz <azeek.school21@gmail.com>
2022-05-21 02:04:00 +0300

    commit

---
+++ clean_cache.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+rm -rf ~/Library/Caches/*
+
+rm -rf ~/Library/42_cache
+
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
